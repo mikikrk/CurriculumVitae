17d9ae6f1bfb983d34bd94c9725af8228785eb81
miki_n <Nzl123?Psz456>
2017-06-10 18:23:52 +0200

    Prepared model

--- app/src/main/java/com/example/mnowak/cirriculumvitae/PersonalDataViewModel.java
+++ app/src/main/java/com/example/mnowak/cirriculumvitae/PersonalDataViewModel.java
@@ -4,6 +4,12 @@ import android.arch.lifecycle.ViewModel;
 
 public class PersonalDataViewModel extends ViewModel {
 
+    public String name;
+    public String position;
+    public String bithDate;
+    public String email;
+    public String location;
 
+    public String photoUrl;
 
 }
